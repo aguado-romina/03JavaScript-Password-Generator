66b4a9977fe0e86f9a3e151bf27ec6327e0f7d9d
Romina Aguado <rominaaguado@Rominas-MacBook-Pro.local>
2020-10-14 18:08:21 -0500

    added functions

--- script.js
+++ script.js
@@ -13,11 +13,46 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
+// Romina Work //
+
 let totalCharacters = prompt ("How many characters do you want?");
 let lowerCase = prompt ("Do you want lower case?");
 let upperCase = prompt ("Do you want upper case?");
 let specialCharacter = prompt ("Do you want special characters?");
 
 
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
 
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
 
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = ' !@#$%^&*(){}[]=<>/,.';
+  return symbols [Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomLower());
+
+console.log(getRandomUpper());
+
+console.log(getRandomNumber());
+
+console.log(getRandomSymbol());
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+}
+
+const 
